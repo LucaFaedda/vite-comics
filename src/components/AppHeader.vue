0967ad1926138701e4d44bbeeb745d53a8f52a18
LucaFaedda <lucakarl91@gmail.com>
2023-01-11 17:30:31 +0100

    + array appHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,63 @@
 <script>
   export default {
-      name: 'AppHeader'
+      name: 'AppHeader',
+      data(){
+        return{
+          listaHeader: [
+            {
+              nome: 'characters',
+              status: false,
+              url: '#'
+            },
+            {
+              nome: 'comics',
+              status: true,
+              url: '#'
+            },{
+              nome: 'movies',
+              status: false,
+              url: '#'
+            },
+            {
+              nome: 'tv',
+              status: false,
+              url: '#'
+            },
+            {
+              nome: 'games',
+              status: false,
+              url: '#'
+            },
+            {
+              nome: 'collectibles',
+              status: false,
+              url: '#'
+            },
+            {
+              nome: 'videos',
+              status: false,
+              url: '#'
+            },
+            {
+              nome: 'fans',
+              status: false,
+              url: '#'
+            },
+            {
+              nome: 'news',
+              status: false,
+              url: '#'
+            },
+            {
+              nome: 'shop',
+              status: false,
+              url: '#'
+            },
+  
+          ]
+
+        }
+      }
   }
 </script>
 <template >
